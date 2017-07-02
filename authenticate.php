115f05823ca7b742ea09bb4d5c1cec1c6e0abc6d
kevin-hartwig <coemen@gmail.com>
2017-07-02 11:20:54 -0400

    Updated login

--- authenticate.php
+++ authenticate.php
@@ -3,6 +3,55 @@
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    $password = sha1($password);
+
+    /*** connect to database ***/
+    /*** mysql hostname ***/
+    $mysql_hostname = 'localhost';
+
+    /*** mysql username ***/
+    $mysql_username = 'root';
+
+   /*** mysql password ***/
+    $mysql_password = '';
+
+    /*** database name ***/
+    $mysql_dbname = 'Project6';
+
+    /* New database object */
+    $dbh = new PDO(
+        "mysql:host=$mysql_hostname;dbname=$mysql_dbname",
+        $mysql_username,
+        $mysql_password
+    );
+
+    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $query = 'SELECT username,password FROM memberInfo
+              WHERE username = :username AND password = :password';
+    $statement = $dbh->prepare($query);
+    $params = [
+        'username' => $username,
+        'password' => $password
+    ];
+    $result = $statement->execute($params);
+    var_dump($result);
+    echo "<br />";
+    $error = $dbh->errorInfo()[2];
+    var_dump($error);
+
+    $result = $statement->fetchColumn();
+
+    if ($result == false) {
+        echo "Login Failed";
+    }
+    else {
+        echo "Login Succeeded";
+        $_SESSION['username']=$username;
+        header("Location: member.php");
+
+    }
+/*
     if($username&&$password) {
         if($username=="Admin" && $password=="pass") {
             $_SESSION['username']=$username;
@@ -13,5 +62,5 @@
         }
     } else {
         header("Location: index.html");
-    }
+    }*/
  ?>
--- request_access.php
+++ request_access.php
@@ -1,13 +1,52 @@
 <?php
     $email = $_POST['email'];
+    $username = $_POST['username_signup'];
     $password = $_POST['password_signup'];
     $password2 = $_POST['passwordrepeat_signup'];
     $bio = $_POST['bio'];
 
-    echo "Email: " . $email . "<br />";
+    echo "Your Username: " . $username . "<br />";
+    echo "Your Email: " . $email . "<br />";
     echo "Password: " . $password . "<br />";
-    echo "Password(2): " . $password2 . "<br />";
+    $password = sha1($password);
+    echo "Password after encryption: " . $password . "<br />";
     echo "bio: " . $bio . "<br />";
 
     echo "<a href='logout.php'>Click</a> to return home.";
+
+    /*** connect to database ***/
+    /*** mysql hostname ***/
+    $mysql_hostname = 'localhost';
+
+    /*** mysql username ***/
+    $mysql_username = 'root';
+
+   /*** mysql password ***/
+    $mysql_password = '';
+
+    /*** database name ***/
+    $mysql_dbname = 'Project6';
+
+    /* New database object */
+    $dbh = new PDO(
+        "mysql:host=$mysql_hostname;dbname=$mysql_dbname",
+        $mysql_username,
+        $mysql_password
+    );
+
+    $query = 'INSERT INTO memberInfo (username,password,email,bio)
+              VALUES (:username,:password,:email,:bio)';
+    $statement = $dbh->prepare($query);
+    $params = [
+        'username' => $username,
+        'password' => $password,
+        'email'    => $email,
+        'bio'      => $bio
+    ];
+    $result = $statement->execute($params);
+    var_dump($result);
+    echo "<br />";
+    $error = $dbh->errorInfo()[2];
+    var_dump($error);
+
 ?>
