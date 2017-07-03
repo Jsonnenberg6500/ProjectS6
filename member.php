777ce83ea97b72360f5ad376e352f4769ae6a262
oovi77 <oovi77@gmail.com>
2017-07-03 09:34:53 -0700

    added stuff222

--- member.php
+++ member.php
@@ -4,7 +4,7 @@
     if (isset($_SESSION['username'])) {
         echo "Welcome, " . $_SESSION['username'] . "!<br />";
 
-        echo "<p>This is where our elevator control panel will be.</p>";
+        require 'control.html';
 
         echo "Click to <a href='logout.php'>Logout</a>";
     } else {
