edb0de78e3e6330f3f5b2232c608d0099de74bf8
Jsonnenberg6500 <sonnenberg.james96@gmail.com>
2017-07-17 14:51:28 -0400

    Logbook James

--- member.php
+++ member.php
@@ -58,36 +58,7 @@
 
         require 'control.php';
 
-
-
-        //JAMES MAKING EDITS FROM HERE DOWN
-        echo "<h1>Entire Content of the clientRequests table</h1>";
-        $db = new PDO(
-            'mysql:host=127.0.0.1;dbname=elevator',
-            'root',
-            ''
-        );
-        $rows = $db->query('SELECT * FROM clientRequests ORDER BY requestID desc');
-        foreach($rows as $row){
-            for($i=0; $i<sizeof($row)/2; $i++){
-                echo $row[$i] . " | ";
-            }
-            echo "<br />";
-        }
-
-?>
-    <h2>EDIT data into the database using the form below!</h2>
-    <form action="member.php" method="POST">
-        status: <input type="text" name="status" /><br />
-        <input type="submit" value="Add to database" />
-    </form>
-
-    <form action="member.php" method="POST">
-        requestID: <input type="submit" name="requestID" /><br />
-    </form>
-
-<?php
-        echo "Click to <a href='logout.php'>Logout</a>";
+    echo "Click to <a href='logout.php'>Logout</a>";
     }
     else {
         echo "<p>You must be logged in!</p>";
