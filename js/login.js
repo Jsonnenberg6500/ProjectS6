7a46bfcf8517ba2124798fadac69faea7d3aa810
kevin-hartwig <coemen@gmail.com>
2017-07-23 16:56:23 -0400

    Fixed member.php linking

--- js/login.js
+++ js/login.js
@@ -56,9 +56,6 @@ function checkCredentials(){
           if (this.responseText == "fail") {/*Do nothing*/}
           else if (this.responseText == "ok") {
               modifyNavbar_members();
-              document.getElementById('loginform').innerHTML = "<div class=\"container-fluid text-center bg-1\"><b>Login successful!</b></div>\
-                </br><ul><li><a href=\"member.php\">Click here</a> to go to the elevator control panel</li>\
-                    <li><a href=\"index.php\">Click here</a> to return to the homepage</li></ul>";
           }
       }
   };
@@ -81,10 +78,10 @@ function modifyNavbar_members(){
             if (this.responseText == "fail") {/*Do nothing*/}
             else {
                 document.getElementById('memberGreeting').innerHTML = "<a><em style=\"text-transform:none\">Hello, <b style=\"text-transform:lowercase\">" +this.responseText+ "</b></em>!</a>";
-                document.getElementById('memberDropdown').innerHTML = "<li><a href=\"control.php\"><span class=\"glyphicon glyphicon-modal-window\"></span> &nbsp; Elevator UI</a></li> \
+                document.getElementById('memberDropdown').innerHTML = "<li><a href=\"member.php\"><span class=\"glyphicon glyphicon-modal-window\"></span> &nbsp; Elevator UI</a></li> \
                 <li><a href=\"php/logout.php\"><span class=\"glyphicon glyphicon-log-out\"></span>  &nbsp; Log Out</a></li>";
                 document.getElementById('loginform').innerHTML = "<div class=\"container-fluid text-center bg-1\"><b>Login successful!</b></div>\
-                  </br><ul><li><a href=\"php/members.php\">Click here</a> to go to the elevator control panel</li>\
+                  </br><ul><li><a href=\"member.php\">Click here</a> to go to the elevator control panel</li>\
                       <li><a href=\"index.php\">Click here</a> to return to the homepage</li></ul>";
             }
         }
